0812fd1e4a11444ac3fdc7b2906480762ebaca84
Kaje19 <106543155+Kaje19@users.noreply.github.com>
2024-08-22 10:30:38 +0800

    First change

---
+++ resources/js/components/Signup.jsx
@@ -0,0 +1,18 @@
+import react from 'react';
+import ReactDom from 'react-dom/client';
+
+export default function Signup() {
+
+    return (
+        <div>
+            <h1>Welcome to Nix Production and Events System</h1>
+        <h1>Welcome to Nix Production and Events System</h1>
+        
+        </div>
+        
+    );
+}
+
+const container = document.getElementById('sign-up');
+const root = ReactDom.createRoot(container);
+root.render(<Signup/>);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nix Production and Events System</title>
+</head>
+<body>
+    
+<div id="sign-up"></div>
+
+@viteReactRefresh
+@vite('resources/js/app.js')
+
+</body>
+</html>
